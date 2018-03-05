d1ccd327ac7ea776e298e7722b2bba5790f7d22b
cheedoong <cheedoong@acm.org>
2018-03-05 21:00:27 +0800

    added pdf info

---
+++ Make-mingw32-cn.bat
@@ -0,0 +1,5 @@
+pause
+mingw32-make utf8-cn distclean
+mingw32-make utf8-cn
+mingw32-make utf8-cn clean
+pause
---
+++ Make-windows-cn.bat
@@ -0,0 +1,7 @@
+pause
+xelatex utf8-cn
+bibtex utf8-cn
+xelatex utf8-cn
+xelatex utf8-cn
+Make_clean
+pause
